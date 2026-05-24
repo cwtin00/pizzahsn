5e549145b8f7e03507adae0ae19bf5e64a5cdae4
cwtin00 <vorildwixeend@gmail.com>
2026-05-25 00:20:37 +0300

    Add files via upload

--- admin.js
+++ admin.js
@@ -8,6 +8,25 @@ import {
 } from "./firebase.js";
 
 
+import {
+    getAuth,
+    onAuthStateChanged
+} from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const auth = getAuth();
+
+onAuthStateChanged(auth,(user)=>{
+
+    if(!user){
+
+        window.location.href =
+        "login.html";
+
+    }
+
+});
+
 // AKTİF MASA
 
 let currentTable = null;
